fd90f6043a5667ff4b2f533d72d78e1ff4727954
t5w0rd <lightning_0721@163.com>
2019-07-19 18:52:13 +0800

    TextList支持最大显示行数

--- Assets/Prefabs/TextList.cs
+++ Assets/Prefabs/TextList.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class TextList : MonoBehaviour {
     public GameObject _content;
     public GameObject _itemTemplate;
+    public int _maxLines = 100;
+
+    protected LinkedList<GameObject> _items;
 
     void Awake() {
+        _items = new LinkedList<GameObject>();
         _itemTemplate.SetActive(false);
     }
 
@@ -19,6 +24,11 @@ public class TextList : MonoBehaviour {
         item.SetActive(true);
         if (updateLayout == true) {
             UpdateLayout();
+        }
+        _items.AddLast(item);
+        while (_items.Count > _maxLines) {
+            Destroy(_items.First.Value);
+            _items.RemoveFirst();
         }
     }
 
