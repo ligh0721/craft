c947bfc5cc7a7556bcc2c36ebf1fa4723112cb0b
t5w0rd <lightning_0721@163.com>
2019-07-22 19:58:16 +0800

    UI PropertyItem, UnitItem, GroupItem

---
+++ Assets/Prefabs/GroupItem.cs
@@ -0,0 +1,29 @@
+﻿public class GroupItem : PropertyItem {
+    void Start() {
+        SetTitle("测试组", "2");
+        AddGroupItem("单位1", "Lv.1");
+        AddGroupItem("单位2", "Lv.2");
+        AddGroupItem("单位3", "Lv.3");
+        //AddProperty("DEF", "20");
+        //AddProperty("ATK", "120");
+        //AddProperty("DEF", "20");
+        //AddProperty("ATK", "120");
+        //AddProperty("DEF", "20");
+        UpdateLayout();
+    }
+
+    public void AddGroupItem(string name, string value) {
+        var item = NewDropListItem();
+        UnitItem unitItem = item.GetComponent<UnitItem>();
+        unitItem.SetTitle(name, value);
+        unitItem.SetUpdateLayoutCallback(UpdateLayout);
+        unitItem.AddUnitItem("HP", "100/100");
+        unitItem.AddUnitItem("ATK", "120");
+        unitItem.AddUnitItem("HP", "100/100");
+        unitItem.AddUnitItem("ATK", "120");
+        unitItem.AddUnitItem("HP", "100/100");
+        unitItem.AddUnitItem("ATK", "120");
+
+        AddDropListItem(item);
+    }
+}
---
+++ Assets/Prefabs/UnitItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.UI;
+
+public class UnitItem : PropertyItem {
+    void Start() {
+        SetTitle("测试单位", "Lv.1");
+        AddUnitItem("HP", "100/100");
+        AddUnitItem("ATK", "120");
+        ////AddProperty("DEF", "20");
+        ////AddProperty("ATK", "120");
+        ////AddProperty("DEF", "20");
+        ////AddProperty("ATK", "120");
+        ////AddProperty("DEF", "20");
+        //UpdateLayout();
+    }
+
+    public void AddUnitItem(string name, string value) {
+        var item = NewDropListItem();
+		var txtName = item.transform.Find("Name").GetComponent<Text>();
+        var txtValue = item.transform.Find("Value").GetComponent<Text>();
+        txtName.text = name;
+        txtValue.text = value;
+
+        AddDropListItem(item);
+    }
+}
