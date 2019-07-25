27823916f20b820ceb7c9c81790a2b609ba218a6
t5w0rd <lightning_0721@163.com>
2019-07-26 00:51:22 +0800

    LayerManager注册场景切换事件

--- Assets/Scripts/Unity/LayerManager.cs
+++ Assets/Scripts/Unity/LayerManager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class LayerManager {
     static GameObject _currentLayer;
 
     static LayerManager() {
+        SceneManager.sceneLoaded += OnLoadScene;
     }
+
+    static void OnLoadScene(Scene scene, LoadSceneMode mode) => _currentLayer = null;
 
     public static void LoadLayer(GameObject layer) {
         layer.SetActive(true);
